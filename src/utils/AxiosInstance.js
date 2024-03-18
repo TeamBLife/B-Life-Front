28db4f34c623cffa623eb30711150b443988f8f2
Myolang <myohanmyolang@gmail.com>
2024-03-18 10:38:02 +0900

    axios https 인증 되지 않아도 되도록 수정

--- src/utils/AxiosInstance.js
+++ src/utils/AxiosInstance.js
@@ -1,6 +1,10 @@
 import axios from "axios";
+import https from 'https'
 
 
 export default axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
-})
+  httpsAgent: new https.Agent({
+    rejectUnauthorized: false,
+  }),
+});
